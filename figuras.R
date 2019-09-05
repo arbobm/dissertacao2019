5784cd3bd8a337949d2fcd44269121d4aed8cb52
Bruna Arbo <brunameneses@gmail.com>
2019-09-05 20:08:29 -0300

    figuras inseridas e referenciadas até resultados :)

--- figuras.R
+++ figuras.R
@@ -1,2 +1,59 @@
 ## @knitr areaestudo
 knitr::include_graphics("figuras/01_AreaDeEstudoFisionmiascampestres_meiaA4_v2.jpg")
+#legenda "Área de estudo abordada no diagnóstico de pressão ambiental no bioma Pampa. Fisionomias campestres: CArb = Campo Arbustivo; CAre = Campo com Areais; CBDB = Campo com Barba-de-Bode; CEsp = Campo com Espinilho; CSR = Campo com Solos Rasos; CGra = Campo Graminoso; CMAC = Campo Misto com Andropogôneas e Compostas; CMCO = Campo Misto do Cristalino Oriental; FE = Floresta Estacional."
+
+## @knitr locbacias3ordem
+knitr::include_graphics("figuras/02_AreaDeEstudoSubBacias_meiaA4_v2.jpg")
+#legenda Localização das bacias de 3ª ordem utilizadas nas análises. CArb = Campo Arbustivo; CAre = Campo com Areais; CBDB = Campo com Barba-de-Bode; CEsp = Campo com Espinilho; CSR = Campo com Solos Rasos; CGra = Campo Graminoso; CMAC = Campo Misto com Andropogôneas e Compostas; CMCO = Campo Misto do Cristalino Oriental; FE = Floresta Estacional.
+
+## @knitr medidas
+knitr::include_graphics("figuras/03_medidas.jpg")
+#legenda Ilustração das 19 medidas morfométricas utilizadas para calcular os 13 atributos funcionais relacionados ao uso do hábitat e comportamento alimentar. SL = comprimento padrão; BD = profundidade do corpo; BW = largura do corpo; HL = comprimento da cabeça; HD = profundidade da cabeça; EH = altura do olho; MH = altura da boca; PfL = comprimento da nadadeira peitoral; PfH = atura da nadadeira peitoral; PfP = posição da nadadeira peitoral; VfL = comprimento da nadadeira ventral; VfH = altura da nadadeira ventral; DfL = comprimento da nadadeira dorsal; DfH = altura da nadadeira dorsal; CpL = comprimento do pedúnculo caudal; CpH = altura do pedúnculo caudal; CpW = largura do pedúnculo caudal; CfL = comprimento da nadadeira caudal; e CfH = altura da nadadeira caudal.
+
+## @knitr pressoes
+knitr::include_graphics("figuras/04_Pressoes_v2.jpg")
+#legenda Distribuição espacial dos indicadores de pressão ambiental com os limites das fisionomias campestres encontradas no bioma Pampa. Ver Figura 1Figura 1 para identificação das fisionomias campestres.
+
+## @knitr boxplotsfisios
+readLines("figuras/05_boxplots.R")
+#Variação dos indicadores de pressão ambiental por fisionomia campestre. Todas as variáveis variam entre 0 e 1, exceto Via (cujo valor máximo é 2,65) e Gado (cujo valor máximo é 1,92). Agr = áreas agrícolas; Urb = áreas urbanas; EA = espelhos d'água; Min = mineração; Via =  rede viária; Gado = pecuária; CArb = Campo Arbustivo; CAre = Campo com Areais; CBDB = Campo com Barba-de-Bode; CEsp = Campo com Espinilho; CSR = Campo com Solos Rasos; CGra = Campo Graminoso; CMAC = Campo Misto com Andropogôneas e Compostas; CMCO = Campo Misto do Cristalino Oriental; FE = Floresta Estacional.
+
+## @knitr resultscen2
+knitr::include_graphics("figuras/06_Results_Bacias_cenario2.jpg")
+#Distribuição espacial do índice global de pressão ambiental resultante do cenário 2.
+
+## @knitr hist-indpressao
+knitr::include_graphics("figuras/07_histograma_indpressao.png")
+#Distribuição dos índices de pressão ambiental nos cinco cenários para o bioma Pampa. A linha cinza representa a porcentagem acumulada de bacias.
+
+## @knitr hist-indpressfisios
+knitr::include_graphics("figuras/08_histogramas cenario 2.png")
+#Frequência de bacias de 3a ordem ao longo do gradiente de pressão ambiental, de acordo com o cenário 2, em cada subunidade regional do bioma Pampa. A linha cinza representa a porcentagem acumulada de bacias. CArb = Campo arbustivo, CAre = campo com areais, CBDB = Campo com Barba-de-bode, CEsp = Campo com espinilho, CSR = campo com solos rasos, CGra = Campo Graminoso, CMAC = Campo misto com Andropogôneas e Compostas, CMCO = Campo misto do cristalino oriental, FE = Floresta Estacional. 
+
+## @knitr RDAtax
+knitr::include_graphics("figuras/09_RDA_PA3.png")
+#Análise de redundância realizada entre a composição taxonômica de espécies de peixes e os indicadores de pressão. Os nomes das espécies estão abreviados e suas correspondências encontram-se na Tabela 9.
+
+## @knitr RiqRar-Agr
+knitr::include_graphics("figuras/10_plot_riqrar_Agr.png")
+#Relação entre Agricultura (%) e riqueza taxonômica. As linhas pontilhadas representam o intervalo de confiança 95%.
+
+## @knitr RiqFunc-Cen
+knitr::include_graphics("figuras/11_plot_riquezafuncionalXcenarios.png")
+#Riqueza funcional em relação aos índices globais de pressão ambiental nos cinco cenários.
+
+## @knitr ImplicPior
+knitr::include_graphics("figuras/12_Implicações_piores.jpg")
+#Proporção da área de bacias hidrográficas de 3ª ordem mais pressionadas (índice global maior que 0,6) nas fisionomias campestres.
+
+## @knitr ImplicRef
+knitr::include_graphics("figuras/13_Implicações_referencias.jpg")
+#Proporção da área de bacias hidrográficas de 3ª ordem de referência nas fisionomias campestres.
+
+## @knitr ImplicInterm
+knitr::include_graphics("figuras/14_Implicações_intermediarias.jpg")
+#Proporção da área de bacias hidrográficas de 3ª ordem com grau de pressão intermediária (índice global entre 0,4 e 0,6) nas fisionomias campestres.
+
+## @knitr MapaCompl
+knitr::include_graphics("figuras/15_Implicações_mapacompleto.jpg")
+#Bacias hidrográficas de 3ª ordem classificadas conforme grau de pressão no bioma Pampa. 
---
+++ figuras/05_boxplots.R
@@ -0,0 +1,105 @@
+arbustivoPatos         <- read.table("C:/SIG/laboratorio/01_mestrado/capituloUM/01_ShapesEmCambuim/Bacias/ordem3/bacias_por_fisionomias/ArbustivoPatos.txt",h=T)
+arbustivoUruguai       <- read.table("C:/SIG/laboratorio/01_mestrado/capituloUM/01_ShapesEmCambuim/Bacias/ordem3/bacias_por_fisionomias/ArbustivoUruguai.txt",h=T)
+areaisPatos            <- read.table("C:/SIG/laboratorio/01_mestrado/capituloUM/01_ShapesEmCambuim/Bacias/ordem3/bacias_por_fisionomias/AreaisPatos.txt",h=T)
+barbadebodePatos       <- read.table("C:/SIG/laboratorio/01_mestrado/capituloUM/01_ShapesEmCambuim/Bacias/ordem3/bacias_por_fisionomias/BarbadeBodePatos.txt",h=T)
+barbadebodeUruguai     <- read.table("C:/SIG/laboratorio/01_mestrado/capituloUM/01_ShapesEmCambuim/Bacias/ordem3/bacias_por_fisionomias/BarbadeBodeUruguai.txt",h=T)
+espinilhoUruguai       <- read.table("C:/SIG/laboratorio/01_mestrado/capituloUM/01_ShapesEmCambuim/Bacias/ordem3/bacias_por_fisionomias/EspinilhoUruguai.txt",h=T)
+solosrasosUruguai      <- read.table("C:/SIG/laboratorio/01_mestrado/capituloUM/01_ShapesEmCambuim/Bacias/ordem3/bacias_por_fisionomias/SolosRasosUruguai.txt",h=T)
+graminosoPatos         <- read.table("C:/SIG/laboratorio/01_mestrado/capituloUM/01_ShapesEmCambuim/Bacias/ordem3/bacias_por_fisionomias/GraminosoPatos.txt",h=T)
+graminosoUruguai       <- read.table("C:/SIG/laboratorio/01_mestrado/capituloUM/01_ShapesEmCambuim/Bacias/ordem3/bacias_por_fisionomias/GraminosoUruguai.txt",h=T)
+mistoandropcompPatos   <- read.table("C:/SIG/laboratorio/01_mestrado/capituloUM/01_ShapesEmCambuim/Bacias/ordem3/bacias_por_fisionomias/MistoAndropCompPatos.txt",h=T)
+mistoandropcompUruguai <- read.table("C:/SIG/laboratorio/01_mestrado/capituloUM/01_ShapesEmCambuim/Bacias/ordem3/bacias_por_fisionomias/MistoAndropCompUruguai.txt",h=T)
+cristorientalPatos     <- read.table("C:/SIG/laboratorio/01_mestrado/capituloUM/01_ShapesEmCambuim/Bacias/ordem3/bacias_por_fisionomias/CrisOrientalPatos.txt",h=T)
+outrosPatos            <- read.table("C:/SIG/laboratorio/01_mestrado/capituloUM/01_ShapesEmCambuim/Bacias/ordem3/bacias_por_fisionomias/OutrosPatos.txt",h=T)
+
+
+rownames(arbustivoPatos)        <- arbustivoPatos[,"COD_B"];         arbustivoPatos         <- arbustivoPatos[,-1]
+rownames(arbustivoUruguai)      <- arbustivoUruguai[,"COD_B"];       arbustivoUruguai       <- arbustivoUruguai[,-1]
+rownames(areaisPatos)           <- areaisPatos[,"COD_B"];            areaisPatos            <- areaisPatos[,-1]
+rownames(barbadebodePatos)      <- barbadebodePatos[,"COD_B"];       barbadebodePatos       <- barbadebodePatos[,-1]
+rownames(barbadebodeUruguai)    <- barbadebodeUruguai[,"COD_B"];     barbadebodeUruguai     <- barbadebodeUruguai[,-1]
+rownames(espinilhoUruguai)      <- espinilhoUruguai[,"COD_B"];       espinilhoUruguai       <- espinilhoUruguai[,-1]
+rownames(solosrasosUruguai)     <- solosrasosUruguai[,"COD_B"];      solosrasosUruguai      <- solosrasosUruguai[,-1]
+rownames(graminosoPatos)        <- graminosoPatos[,"COD_B"];         graminosoPatos         <- graminosoPatos[,-1]
+rownames(graminosoUruguai)      <- graminosoUruguai[,"COD_B"];       graminosoUruguai       <- graminosoUruguai[,-1]
+rownames(mistoandropcompPatos)  <- mistoandropcompPatos[,"COD_B"];   mistoandropcompPatos   <- mistoandropcompPatos[,-1]
+rownames(mistoandropcompUruguai)<- mistoandropcompUruguai[,"COD_B"]; mistoandropcompUruguai <- mistoandropcompUruguai[,-1]
+rownames(cristorientalPatos)    <- cristorientalPatos[,"COD_B"];     cristorientalPatos     <- cristorientalPatos[,-1]
+rownames(outrosPatos)           <- outrosPatos[,"COD_B"];            outrosPatos            <- outrosPatos[,-1]
+
+
+names(cristorientalPatos)
+arbustivoPatos.f         <- arbustivoPatos[,c("AgricProp","UrbProp",  "AcudeProp","DNPMProp", "ViarioKk2", "DensBOS")]  
+arbustivoUruguai.f       <- arbustivoUruguai[,c("AgricProp","UrbProp",  "AcudeProp","DNPMProp", "ViarioKk2", "DensBOS")] 
+areaisPatos.f            <- areaisPatos[,c("AgricProp","UrbProp",  "AcudeProp","DNPMProp", "ViarioKk2", "DensBOS")] 
+barbadebodePatos.f       <- barbadebodePatos[,c("AgricProp","UrbProp",  "AcudeProp","DNPMProp", "ViarioKk2", "DensBOS")] 
+barbadebodeUruguai.f     <- barbadebodeUruguai[,c("AgricProp","UrbProp",  "AcudeProp","DNPMProp", "ViarioKk2", "DensBOS")] 
+espinilhoUruguai.f       <- espinilhoUruguai[,c("AgricProp","UrbProp",  "AcudeProp","DNPMProp", "ViarioKk2", "DensBOS")] 
+solosrasosUruguai.f      <- solosrasosUruguai[,c("AgricProp","UrbProp",  "AcudeProp","DNPMProp", "ViarioKk2", "DensBOS")] 
+graminosoPatos.f         <- graminosoPatos[,c("AgricProp","UrbProp",  "AcudeProp","DNPMProp", "ViarioKk2", "DensBOS")] 
+graminosoUruguai.f       <- graminosoUruguai[,c("AgricProp","UrbProp",  "AcudeProp","DNPMProp", "ViarioKk2", "DensBOS")] 
+mistoandropcompPatos.f   <- mistoandropcompPatos[,c("AgricProp","UrbProp",  "AcudeProp","DNPMProp", "ViarioKk2", "DensBOS")] 
+mistoandropcompUruguai.f <- mistoandropcompUruguai[,c("AgricProp","UrbProp",  "AcudeProp","DNPMProp", "ViarioKk2", "DensBOS")] 
+cristorientalPatos.f     <- cristorientalPatos[,c("AgricProp","UrbProp",  "AcudeProp","DNPMProp", "ViarioKk2", "DensBOS")] 
+outrosPatos.f            <- outrosPatos[,c("AgricProp","UrbProp",  "AcudeProp","DNPMProp", "ViarioKk2", "DensBOS")] 
+
+
+colnames(arbustivoPatos.f)[c(1:6)]        <- c("Agr","Urb","EA","Min","Via","Gado") 
+colnames(arbustivoUruguai.f)[c(1:6)]      <- c("Agr","Urb","EA","Min","Via","Gado")
+colnames(areaisPatos.f)[c(1:6)]           <- c("Agr","Urb","EA","Min","Via","Gado")
+colnames(barbadebodePatos.f)[c(1:6)]      <- c("Agr","Urb","EA","Min","Via","Gado")
+colnames(barbadebodeUruguai.f)[c(1:6)]    <- c("Agr","Urb","EA","Min","Via","Gado")
+colnames(espinilhoUruguai.f)[c(1:6)]      <- c("Agr","Urb","EA","Min","Via","Gado")
+colnames(solosrasosUruguai.f)[c(1:6)]     <- c("Agr","Urb","EA","Min","Via","Gado")
+colnames(graminosoPatos.f)[c(1:6)]        <- c("Agr","Urb","EA","Min","Via","Gado")
+colnames(graminosoUruguai.f)[c(1:6)]      <- c("Agr","Urb","EA","Min","Via","Gado")
+colnames(mistoandropcompPatos.f)[c(1:6)]  <- c("Agr","Urb","EA","Min","Via","Gado")
+colnames(mistoandropcompUruguai.f)[c(1:6)]<- c("Agr","Urb","EA","Min","Via","Gado")
+colnames(cristorientalPatos.f)[c(1:6)]    <- c("Agr","Urb","EA","Min","Via","Gado")
+colnames(outrosPatos.f)[c(1:6)]           <- c("Agr","Urb","EA","Min","Via","Gado")
+
+
+library(vegan)
+max(decostand(arbustivoPatos.f,method = "standardize"))
+decostand(arbustivoPatos.f,method = "standardize")==max(decostand(arbustivoPatos.f,method = "standardize"))
+?boxplot
+par(mfrow=c(5,3),mai=c(0.4,0.4,0.2,0.1))
+#par(mfrow=c(1,2))
+
+
+boxplot(arbustivoPatos.f ,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "CArb - Patos")
+boxplot(arbustivoUruguai.f ,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "CArb - Uruguai")
+boxplot(areaisPatos.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "CAre - Patos")
+boxplot(barbadebodePatos.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "CBDB - Patos")
+boxplot(barbadebodeUruguai.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "CBDB - Uruguai")
+boxplot(espinilhoUruguai.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "CEsp - Uruguai")
+boxplot(solosrasosUruguai.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "CSR - Uruguai")
+boxplot(graminosoPatos.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "CGra - Patos")
+boxplot(graminosoUruguai.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "CGra - Uruguai")
+boxplot(mistoandropcompPatos.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "CMAC - Patos")
+boxplot(mistoandropcompUruguai.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "CMAC - Uruguai")
+boxplot(cristorientalPatos.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "CMCO - Patos")
+boxplot(outrosPatos.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "FE - Patos")
+# 
+# ?png
+# png(file = "myplot.png", bg = "transparent", width=19, height = 25, units="cm" , res=500)
+# par(mfrow=c(5,3),mai=c(0.4,0.4,0.2,0.1))
+# #par(mfrow=c(1,2))
+# 
+# 
+# boxplot(arbustivoPatos.f ,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "Arbustivo - Patos")
+# boxplot(arbustivoUruguai.f ,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "Arbustivo - Uruguai")
+# boxplot(areaisPatos.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "Areais - Patos")
+# boxplot(barbadebodePatos.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "BarbaBode - Patos")
+# boxplot(barbadebodeUruguai.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "BarbaBode - Uruguai")
+# boxplot(espinilhoUruguai.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "Espinilho - Uruguai")
+# boxplot(solosrasosUruguai.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "SolosRasos - Uruguai")
+# boxplot(graminosoPatos.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "Graminoso - Patos")
+# boxplot(graminosoUruguai.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "Graminoso - Uruguai")
+# boxplot(mistoandropcompPatos.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "MistoAndropComp - Patos")
+# boxplot(mistoandropcompUruguai.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "MistoAndrop - Uruguai")
+# boxplot(cristorientalPatos.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "Cristoriental - Patos")
+# boxplot(outrosPatos.f,boxwex=0.25,xlim=c(0.8,6.3),ylim=c(0,2.5), main = "Floresta estacional - Patos")
+# 
+# dev.off()
+# getwd()
